0a5331f34d469b00faf25b906b609ed56e13fd99
Jake Knudsen <jakeknudsen@Jakes-MacBook-Pro.local>
2020-03-12 11:36:00 -0700

    Will create DB using MySQL populate location table and dates table with mockData

--- database/index.js
+++ database/index.js
@@ -1,25 +1,48 @@
+/* eslint-disable no-shadow */
 const mysql = require('mysql2');
-// const sequelize = require('sequelize');
+const data = require('./mockData');
 // Create a database connection and export it from this file.
 // You will need to connect with the user "root", no password,
 // and to the database "chat".
 const dbConnection = mysql.createConnection({
   user: 'root',
-  password: 'password',
-  database: 'availability'
+  password: 'password'
 });
 
 dbConnection.connect(err => {
   if (err) {
     throw err;
   }
-  // eslint-disable-next-line no-shadow
-  dbConnection.query(`CREATE DATABASE availability`, err => {
-    if (err) {
-      throw err;
+  // eslint-disable-next-line no-console
+  dbConnection.query('CREATE DATABASE availability;', err => {
+    if (err) throw err;
+  });
+  dbConnection.query('USE availability;', err => {
+    if (err) throw err;
+  });
+  dbConnection.query(
+    'CREATE TABLE Location (id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, name varchar(45))',
+    err => {
+      if (err) throw err;
     }
-    // eslint-disable-next-line no-console
-    console.log('connected');
+  );
+  dbConnection.query(
+    'CREATE TABLE Dates (id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, date DATE)',
+    err => {
+      if (err) throw err;
+    }
+  );
+  data.forEach(loc => {
+    dbConnection.query(`INSERT INTO Location (name) VALUES ('${loc}')`);
+  });
+  dbConnection.query(
+    `CREATE PROCEDURE fill_calendar(start_date DATE, end_date DATE) BEGIN DECLARE crt_date DATE; SET crt_date=start_date; WHILE crt_date < end_date DO INSERT INTO Dates (date) VALUES (crt_date); SET crt_date = ADDDATE(crt_date, INTERVAL 1 DAY); END WHILE; END`,
+    err => {
+      if (err) throw err;
+    }
+  );
+  dbConnection.query(`CALL fill_calendar('2020-03-01', '2020-5-31');`, err => {
+    if (err) throw err;
   });
 });
 
