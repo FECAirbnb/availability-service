86795ebb711f74b426123ce7f94e8dde36a65049
Jake Knudsen <jakeknudsen@Jakes-MacBook-Pro.local>
2020-03-12 14:42:30 -0700

    add join table to DB to keep track of dataset relations

--- database/index.js
+++ database/index.js
@@ -17,33 +17,49 @@ dbConnection.connect(err => {
   dbConnection.query('CREATE DATABASE availability;', err => {
     if (err) throw err;
   });
+
   dbConnection.query('USE availability;', err => {
     if (err) throw err;
   });
+
   dbConnection.query(
     'CREATE TABLE Location (id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, name varchar(45))',
     err => {
       if (err) throw err;
     }
   );
+
   dbConnection.query(
     'CREATE TABLE Dates (id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, date DATE)',
     err => {
       if (err) throw err;
     }
   );
+
   data.forEach(loc => {
     dbConnection.query(`INSERT INTO Location (name) VALUES ('${loc}')`);
   });
+
   dbConnection.query(
     `CREATE PROCEDURE fill_calendar(start_date DATE, end_date DATE) BEGIN DECLARE crt_date DATE; SET crt_date=start_date; WHILE crt_date < end_date DO INSERT INTO Dates (date) VALUES (crt_date); SET crt_date = ADDDATE(crt_date, INTERVAL 1 DAY); END WHILE; END`,
     err => {
       if (err) throw err;
     }
   );
+
   dbConnection.query(`CALL fill_calendar('2020-03-01', '2020-5-31');`, err => {
     if (err) throw err;
   });
-});
 
+  dbConnection.query(
+    `CREATE TABLE Location_Dates (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    Location_id INT REFERENCES Location(id),
+    Dates_id INT REFERENCES Dates(id)
+  )`,
+    err => {
+      if (err) throw err;
+    }
+  );
+});
 module.exports = dbConnection;
